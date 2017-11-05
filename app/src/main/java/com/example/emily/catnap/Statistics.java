5d2bcc941036b0263ec233de06f600dbdbb10649
Stanley Lok <stanlok032996@gmail.com>
2017-11-05 09:22:36 -0500

    Added documentation to Statistics ativity page

--- app/src/main/java/com/example/emily/catnap/Statistics.java
+++ app/src/main/java/com/example/emily/catnap/Statistics.java
@@ -7,6 +7,15 @@ import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.LineGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
 
+/**
+ * Created by Emily on 11/4/2017.
+ *
+ * This class creates a line graph for the Statistics page
+ * to plot the amount of time the user is sleeping, which is
+ * measured by the amount of time their phone screen is off
+ * during the user's specified time interval.
+ */
+
 public class Statistics extends AppCompatActivity {
 
     @Override
@@ -14,9 +23,17 @@ public class Statistics extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
 
+        /**
+         * x - The hour in the day
+         *
+         * y - The amount of sleep the user got
+         */
         int[] x = new int[] {0,1,2,3,4,5,6,7,8,9,10,11,12};
-        int[] y = new int[] {0,0,0,0,0,0,0,7,60,52,0,0,0};
+        int[] y = new int[] {0,15,0,25,5,0,0,7,10,20,2,0,0};
 
+        /**
+         * Graphs and displays the data in a line graph
+         */
         GraphView graph = findViewById(R.id.graph);
         DataPoint[] values = new DataPoint[x.length];
         for (int i =0; i <x.length;i++) {
