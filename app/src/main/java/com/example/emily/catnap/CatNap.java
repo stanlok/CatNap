a4213fe7482006979923313cc1fe34e19791a424
Austin Chow <imr.chow@gmail.com>
2017-11-04 22:40:01 -0400

    Experimented with screen locking

--- app/src/main/java/com/example/emily/catnap/CatNap.java
+++ app/src/main/java/com/example/emily/catnap/CatNap.java
@@ -1,5 +1,6 @@
 package com.example.emily.catnap;
 
+import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -9,17 +10,30 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.app.ActivityManager;
+import android.app.admin.DevicePolicyManager;
+import java.util.Calendar;
 
-public class CatNap extends AppCompatActivity {
+public class CatNap extends AppCompatActivity implements View.OnContextClickListener{
 
     Button customize_button;
     Button statistics_button;
     Button credits_button;
+    public static final int RESULT_ENABLE = 1;
+    DevicePolicyManager devicePolicyManager;
+    ActivityManager activityManager;
+    ComponentName componentName;
+    Calendar currentTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cat_nap);
+        devicePolicyManager = (DevicePolicyManager) getSystemService(DEVICE_POLICY_SERVICE);
+        activityManager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+        componentName = new ComponentName(this, MyAdmin.class);
+        currentTime = Calendar.getInstance().getTimeInMillis();
+
 
         final GlobalVars globalVariable = (GlobalVars) getApplicationContext();
 
@@ -56,6 +70,15 @@ public class CatNap extends AppCompatActivity {
                 startActivity(new Intent(CatNap.this, Credits.class));
             }
         });
+
     }
+        @Override
+    protected void onResume() {
+            super.onResume();
+            boolean isActive = devicePolicyManager.isAdminActive(componentName);
+        }
+        @Override
+    public void
+        }
 
 }
